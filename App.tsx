fd760aecfc49f5ccc5adbbed8bccf0d3414faded
ChrisVilla <cvillarroel@bice.cl>
2021-07-07 12:38:53 -0400

    Added Drawer SideBar component

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { StackNavigator } from './src/navigator/StackNavigator'
+// import { StackNavigator } from './src/navigator/StackNavigator'
+import { SideBar } from './src/navigator/SideBar'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <SideBar />
     </NavigationContainer>
   )
 }
