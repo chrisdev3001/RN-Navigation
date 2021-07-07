1447a3829e21f1c99573f3b925ecc4bff5b2f8dc
ChrisVilla <cvillarroel@bice.cl>
2021-07-07 12:59:38 -0400

    Added conditional drawerType to Drawer Sidebar

--- src/navigator/SideBar.tsx
+++ src/navigator/SideBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useWindowDimensions } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { SettingScreen } from '../screens/SettingScreen';
 import { StackNavigator } from './StackNavigator';
@@ -6,9 +7,11 @@ import { StackNavigator } from './StackNavigator';
 const Drawer = createDrawerNavigator();
 
 export function SideBar() {
+  const { width } = useWindowDimensions()
   return (
     <Drawer.Navigator
       drawerPosition="left"
+      drawerType={width >= 768 ? 'permanent' : 'front'}
     >
       <Drawer.Screen name="StackNavigator" options={{ title: 'Home '}} component={StackNavigator} />
       <Drawer.Screen name="SettingScreen" options={{ title: 'Settings'}} component={SettingScreen} />
