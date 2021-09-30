72013e9c006d3eafe7dc908c17919a641689fb1b
ChrisVilla <cvillarroel@bice.cl>
2021-09-30 17:17:40 -0300

    add ios config icons

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Styles } from '../theme/Styles';
 
 export const Tab1Screen = () => {
+    const { top } = useSafeAreaInsets()
     return (
-        <View style={Styles.globalMargin}>
+        <View style={{
+            ...Styles.globalMargin,
+            marginTop: top + 20
+        }}
+        >
             <Text style={Styles.title}>Tab1 Screen</Text>
             <Icon name="airplane-outline" size={30} color="#900" />
         </View>
