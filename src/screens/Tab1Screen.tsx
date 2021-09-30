2d278c049a4ec4281db94825328764152538e2d2
ChrisVilla <cvillarroel@bice.cl>
2021-09-30 17:03:15 -0300

    add icons and add android config

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Styles } from '../theme/Styles';
 
 export const Tab1Screen = () => {
     return (
-        <View>
-            <Text>Tab1 Screen</Text>
+        <View style={Styles.globalMargin}>
+            <Text style={Styles.title}>Tab1 Screen</Text>
+            <Icon name="airplane-outline" size={30} color="#900" />
         </View>
     )
 }
