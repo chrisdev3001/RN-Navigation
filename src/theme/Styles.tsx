9e4811c9b3ad7a886ac3e8b1663abfe484f0d128
ChrisVilla <cvillarroel@bice.cl>
2021-09-30 15:54:19 -0300

    remove tabs border/elevation

--- src/theme/Styles.tsx
+++ src/theme/Styles.tsx
@@ -1,5 +1,9 @@
 import { StyleSheet } from "react-native";
 
+export const Colors = {
+    primary: '#5856D6'
+}
+
 export const Styles = StyleSheet.create({
     globalMargin: {
         marginHorizontal: 20
